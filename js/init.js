faf93cca071d2e86b74d18288bde855c80b39402
Nicholas Patano <nicholaspatano@gmail.com>
2022-02-01 14:14:01 -0500

    style changes and script

--- js/init.js
+++ js/init.js
@@ -5,3 +5,11 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+//code for auto complete function? pending functionality
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, options);
+  
+});
+
